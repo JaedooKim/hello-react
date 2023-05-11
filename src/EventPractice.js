096d8940ff13442c4568d2e9591082b73938daff
김재두 <kimjd@mz.co.kr>
2023-05-11 19:29:42 +0900

    이벤트 연습

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,13 +1,54 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class EventPractice extends Component {
-    render() {
-        return (
-            <div>
-                <h1>이벤트 연습</h1>
-            </div>
-        )
+  state = {
+    username: '',
+    message: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.username + ': ' + this.state.message);
+    this.setState({
+      username: '',
+      message: '',
+    });
+  };
+
+  handlePress = (e) => {
+    if(e.key === 'Enter') {
+        this.handleClick();
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
+          onChange={this.handleChange}
+          onKeyPress={this.handlePress}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
 }
 
 export default EventPractice;
